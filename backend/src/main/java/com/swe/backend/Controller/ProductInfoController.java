b33bd919a92e4fb1330e005ca5511070fed04b7c
Fraser Coyle <fraseraco@gmail.com>
2025-04-07 23:23:17 -0400

    Added @CrossOrigin annotations to controller classes

--- backend/src/main/java/com/swe/backend/Controller/ProductInfoController.java
+++ backend/src/main/java/com/swe/backend/Controller/ProductInfoController.java
@@ -8,13 +8,11 @@ import com.swe.backend.Service.UserService;
 import com.swe.backend.Views.Views;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = {"/product"})
 public class ProductInfoController {
 
