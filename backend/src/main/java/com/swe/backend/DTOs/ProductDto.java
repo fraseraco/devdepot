fcf40df8376cd33134b7ce9f42ca3b9837a6d981
Fraser Coyle <fraseraco@gmail.com>
2025-04-12 00:27:25 -0400

    quicksave

--- backend/src/main/java/com/swe/backend/DTOs/ProductDto.java
+++ backend/src/main/java/com/swe/backend/DTOs/ProductDto.java
@@ -31,4 +31,16 @@ public class ProductDto implements Serializable {
     String sku;
     @NotNull
     List<Map<String, Object>> specifications;
+
+    public ProductDto(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.category = product.getCategory();
+        this.brand = product.getBrand();
+        this.inventoryQty = product.getInventoryQty();
+        this.price = product.getPrice();
+        this.description = product.getDescription();
+        this.sku = product.getSku();
+        this.specifications = product.getSpecifications();
+    }
 }
