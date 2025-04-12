f16cf312a4259b8406d727dd52515518665fc7c7
Fraser Coyle <fraseraco@gmail.com>
2025-04-11 22:51:10 -0400

    Creating an Admin endpoint for easier management

--- backend/src/main/java/com/swe/backend/Controller/ProductsController.java
+++ backend/src/main/java/com/swe/backend/Controller/ProductsController.java
@@ -1,14 +1,17 @@
 package com.swe.backend.Controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.swe.backend.DTOs.ProductDto;
+import com.swe.backend.DTOs.UserDto;
+import com.swe.backend.DTOs.UserRegistrationDto;
 import com.swe.backend.Entity.Product;
+import com.swe.backend.Entity.User;
 import com.swe.backend.Service.ProductService;
 import com.swe.backend.Views.Views;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +23,13 @@ public class ProductsController {
 
     public ProductsController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ProductDto> registerUser(@RequestBody UserRegistrationDto userRegistrationDto) {
+        User user = userService.registerNewUser(userRegistrationDto);
+        UserDto userDto = new UserDto(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
     }
 
     @JsonView(Views.Public.class)
