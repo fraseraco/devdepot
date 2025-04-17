88f3ded30acc0ea97a707eb1df6b6e8018119338
Fraser Coyle <fraseraco@gmail.com>
2025-04-17 08:45:42 -0400

    oh

--- backend/src/main/java/com/swe/backend/DTOs/CartDto.java
+++ backend/src/main/java/com/swe/backend/DTOs/CartDto.java
@@ -2,18 +2,14 @@ package com.swe.backend.DTOs;
 
 import com.swe.backend.Entity.Cart;
 import com.swe.backend.Entity.CartItem;
-import com.swe.backend.Entity.User;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
