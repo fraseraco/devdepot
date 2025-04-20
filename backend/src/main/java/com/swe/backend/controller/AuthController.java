bebabe880b788adb8ffe8b3d0f52fbf44ad3a891
Fraser Coyle <fraseraco@gmail.com>
2025-04-20 04:11:59 -0400

    removed redundant apis /users/register /users/all

--- backend/src/main/java/com/swe/backend/controller/AuthController.java
+++ backend/src/main/java/com/swe/backend/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,7 +45,7 @@ public class AuthController {
         String token = jwtUtil.generate(
                 authentication.getName(),
                 authentication.getAuthorities().stream()
-                        .map(a -> a.getAuthority())
+                        .map(GrantedAuthority::getAuthority)
                         .toList()
         );
         return ResponseEntity
--- backend/src/main/java/com/swe/backend/controller/UserController.java
+++ backend/src/main/java/com/swe/backend/controller/UserController.java
@@ -19,21 +19,11 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
     public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
     }
-
-    @PostMapping("/register")
-    public ResponseEntity<Authentication> registerUser(@RequestBody UserRegistrationDto userRegistrationDto) {
-        UserDto userDto = userService.registerNewUser(userRegistrationDto, passwordEncoder);
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return ResponseEntity.status(HttpStatus.CREATED).body(authentication);
-    }
-
-
 
     // returns current authenticated user
     @GetMapping("/me")
@@ -44,15 +34,6 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<List<SlimUserDto>> getUsers() {
-        return ResponseEntity.ok(userService.getSlimUsers());
-    }
+    /* @ToDo Allow User to change password */
 
-    // authentication check @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUserByID(@PathVariable Long id) {
-        return userService.getUserById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
 }
