3ccdcd431b4421ee363a43209e00beee234bd652
Fraser Coyle <fraseraco@gmail.com>
2025-04-20 22:53:46 -0400

    cart-fixes

--- backend/src/main/java/com/swe/backend/entity/Cart.java
+++ backend/src/main/java/com/swe/backend/entity/Cart.java
@@ -96,7 +96,7 @@ public class Cart{
                 .filter(ci -> ci.getProduct().equals(product))
                 .findFirst()
                 .orElseGet(() -> {
-                    CartItem ci = new CartItem(product, 0);
+                    CartItem ci = new CartItem(this, product, 0);
                     cartItems.add(ci);
                     return ci;
                 });
